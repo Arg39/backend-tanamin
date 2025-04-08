0529568229168521910b4ada5b2cfee31b300776
Arg39 <pabannu@gmail.com>
2025-04-08 18:18:19 +0800

    fix-certificate

--- resources/views/certificate.blade.php
+++ resources/views/certificate.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <title>Certificate</title>
@@ -12,7 +13,7 @@
         body {
             margin: 0;
             padding: 0;
-            font-family: 'Georgia', serif;
+            font-family: 'Poppins', sans-serif;
         }
 
         .container {
@@ -34,7 +35,7 @@
             position: relative;
             z-index: 1;
             text-align: center;
-            padding: 100px;
+            /* padding: 100px; */
             color: #deebdf;
         }
 
@@ -68,35 +69,68 @@
         .qr-code img {
             width: 100px;
         }
+
+        .year {
+            position: absolute;
+            bottom: 0px;
+            left: 95px;
+            font-weight: 800;
+            font-size: 95px;
+            letter-spacing: 16px;
+            color: #F6FCDF;
+            text-align: center;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: flex-end;
+            gap: 0;
+            /* Tambahkan ini untuk mengatur jarak antar elemen */
+        }
+
+        .year div:first-child {
+            margin-bottom: -80px;
+        }
     </style>
 </head>
+
 <body>
     <div class="container">
-        <img src="{{ public_path('images/certificate-bg-2.png') }}" class="background">
+        {{-- <img src="{{ public_path('images/certificate-bg.png') }}" class="background"> --}}
+        <img src="images/certificate-bg.png" class="background">
 
         <div class="content">
-            {{-- certificate for finish course in PT Tanamin --}}
-            <div style="font-size: 20px; font-weight: bold; margin-bottom: 20px;">Certificate of Completion</div>
-            p
-            <div>This certifies that</div>
-            <div class="name">{{ $user }}</div>
-            has successfully completed the course
-            <div class="course">“{{ $course }}”</div>
-
-            <div class="footer">
-                <div class="signature">
-                    <img src="{{ public_path('images/signature-placeholder.png') }}"><br>
-                    Authorized Signatory
+            <div
+                style="width: 60%; height: 100%; position: absolute; right: 54px; top: 0; text-align: left; word-wrap: break-word; overflow-wrap: break-word; color: #006840; ">
+                <div style="position: absolute; top: 0;">
+                    <p style="font-size: 80px; font-weight: bold; ">CERTIFICATE</p>
+                    <p style="margin-top: -100px; font-weight: regularmont; font-size: 30px;">of completion</p>
+                    <p style="margin-top: -16px; font-weight: regularmont; font-size: 50px; line-height: 65%">
+                        {{ $course }}</p>
+                    <p style="line-height: 65%; font-size: 18px; margin-top: -32px;">instructor <span
+                            style="font-weight: bold;">{{ $insructor }}</span></p>
+                    <p style="line-height: 65%; font-size: 18px; margin-top: 24px;">awarded to</p>
+                    <p style="font-size: 40px; margin-top: -16px; font-weight: bold;">{{ $user }}</p>
                 </div>
-                <div class="qr-code">
-                    <img src="{{ $qr_code }}" alt="QR Code">
+                <div style="position: absolute; bottom: 40px; display: flex; align-items: center;">
+                    <img src="{{ $qr_code }}" alt="QR Code"
+                        style="margin-right: 20px; width: 100px; height: 100px;">
+                    <div>
+                        <p style="margin: 0;">Verified by <span style="font-weight: bold;">PT Tanamin Bumi
+                                Nusantara</span></p>
+                        <p style="margin: 0; font-size: 14px;">Course Code: {{ $certificate_code }}</p>
+                    </div>
                 </div>
             </div>
+        </div>
 
-            <div style="margin-top: 20px; font-size: 14px;">
-                Issued on: {{ $issued_at }}
-            </div>
+        @php
+            $year = \Carbon\Carbon::parse($issued_at)->format('Y');
+        @endphp
+        <div class="year">
+            <div>{{ substr($year, 0, 2) }}</div>
+            <div>{{ substr($year, 2, 2) }}</div>
         </div>
     </div>
 </body>
+
 </html>
