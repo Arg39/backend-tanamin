20621e95d2ac4028ba4a620960ec41a991a97b3f
Arg39 <pabannu@gmail.com>
2025-04-08 18:18:19 +0800

    fix-certificate

--- app/Http/Controllers/Api/CertificateController.php
+++ app/Http/Controllers/Api/CertificateController.php
@@ -11,8 +11,8 @@ class CertificateController extends Controller
 {
     public function generatePdf($id)
     {
-        $id_certificate = 'TESTING-ADS-1234567890';
-        $qrCodeUrl = url("api/certificates/{$id_certificate}/pdf");
+        $id_certificate = 'TC-ADS-1234567890';
+        $qrCodeUrl = env('APP_URL') . "/api/certificates/{$id_certificate}/pdf";
 
         // Path ke logo
         $logoPath = public_path('images/logo.png');
@@ -24,7 +24,7 @@ class CertificateController extends Controller
 
         // Generate QR code dengan logo dan margin
         $qrCodeSvg = QrCode::format('svg')
-            ->size(300)
+            ->size(120)
             ->margin(2) // Tambahkan margin
             ->merge($logoPath, 0.2, true) // Tambahkan logo di tengah
             ->style('square') // Gaya kotak untuk QR code
@@ -33,15 +33,24 @@ class CertificateController extends Controller
         // Encode QR code ke base64
         $qrCodeBase64 = 'data:image/svg+xml;base64,' . base64_encode($qrCodeSvg);
 
+        Pdf::setOptions([
+            'font_dir' => public_path('fonts/'), // Path ke folder font
+            'font_cache' => storage_path('fonts/'), // Cache font
+            'default_font' => 'Poppins', // Default font
+        ]);
+
         // Generate PDF
         $pdf = Pdf::loadView('certificate', [
-            'user' => 'test',
-            'course' => 'test course',
-            'issued_at' => '2023-10-01',
-            'certificate_code' => 'ABC123',
+            'user' => 'Alfian Pabannu',
+            'course' => 'Fullstack Mobile Developer',
+            'insructor' => 'Bacimm darynth Ashornto',
+            'issued_at' => '2025-10-01',
+            'certificate_code' => $id_certificate,
             'qr_code' => $qrCodeBase64,
         ])->setPaper('a4', 'landscape');
 
-        return $pdf->download("certificate.pdf");
+        // see output without download
+        return $pdf->stream("certificate.pdf");
+        // return $pdf->download("certificate.pdf");
     }
 }
