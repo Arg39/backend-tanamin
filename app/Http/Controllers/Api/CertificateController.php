e9d9568ed3ac74d5347444b0ff0ecea042bfaf73
Arg39 <pabannu@gmail.com>
2025-04-14 22:32:11 +0800

    fix docker frankenPHP

--- app/Http/Controllers/Api/CertificateController.php
+++ app/Http/Controllers/Api/CertificateController.php
@@ -20,14 +20,12 @@ class CertificateController extends Controller
             $id_certificate = 'TC-ADS-1234567890';
             $qrCodeUrl = env('APP_URL') . "/api/certificates/{$id_certificate}/pdf";
 
-            // Path ke logo QR
             $logoPath = public_path('images/logo.png');
 
             if (!file_exists($logoPath)) {
                 throw new \Exception("Logo file not found at: {$logoPath}");
             }
 
-            // Path QR temp file (PNG karena DomPDF lebih stabil)
             $qrTempPath = storage_path('app/qr_temp.png');
             QrCode::format('png')
                 ->size(400)
@@ -35,19 +33,16 @@ class CertificateController extends Controller
                 ->merge($logoPath, 0.2, true)
                 ->generate($qrCodeUrl, $qrTempPath);
 
-            // Encode ke base64
             $qrCodeBase64 = 'data:image/png;base64,' . base64_encode(file_get_contents($qrTempPath));
 
-            // PDF option
             Pdf::setOptions([
                 'font_dir' => public_path('fonts/'),
                 'font_cache' => storage_path('fonts/'),
                 'default_font' => 'Poppins',
                 'isHtml5ParserEnabled' => true,
-                'isRemoteEnabled' => true, // penting buat akses asset() URL
+                'isRemoteEnabled' => true,
             ]);
 
-            // Prepare data
             $viewData = [
                 'user' => 'George Smith dawning',
                 'course' => 'Machine Learning for Beginners 2024',
@@ -58,19 +53,17 @@ class CertificateController extends Controller
                 'bg_image' => asset('images/certificate-bg.png'),
             ];
 
-            // Save debug HTML (optional)
             file_put_contents(storage_path('app/pdf_debug.html'), view('certificate', $viewData)->render());
 
-            // Generate PDF
             $pdf = Pdf::loadView('certificate', $viewData)->setPaper('a4', 'landscape');
 
-            // Save PDF
             $filename = 'test_' . Str::random(5) . '.pdf';
             $pdf->save(storage_path("app/{$filename}"));
 
             Log::info("PDF saved to: storage/app/{$filename}");
 
-            return $pdf->stream("certificate.pdf");
+            // return $pdf->stream("certificate.pdf");
+            return $pdf->download("certificate.pdf");
         } catch (Exception $e) {
             Log::error($e->getMessage());
             return response("Internal Server Error: " . $e->getMessage(), 500);
