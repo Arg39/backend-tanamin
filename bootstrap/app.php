27d9fb70d21f44b46b8872866e2bdcbd588f83d1
Arg39 <pabannu@gmail.com>
2025-05-08 00:24:13 +0800

    fix middleware for laravel octane

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,7 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'isAdmin' => AdminMiddleware::class,
-            'disableOctane' => DisableOctane::class,
+            'disable.octane' => DisableOctane::class,
             // 'forceJson' => ForceJsonResponse::class,
         ]);
     })
