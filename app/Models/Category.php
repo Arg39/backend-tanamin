f55b04e1d337613a11ae0d169381a87392e09432
Arg39 <pabannu@gmail.com>
2025-05-08 23:53:39 +0800

    change id using uuid

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
@@ -13,9 +14,25 @@ class Category extends Model
     protected $table = 'categories';
 
     protected $fillable = [
+        'id',
         'name',
         'image',
     ];
+
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    // Automatically generate UUID if not provided
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = (string) Str::uuid();
+            }
+        });
+    }
 
     // Accessor for created_at
     public function getCreatedAtAttribute($value)
